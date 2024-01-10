020954634bdcb475f229d80feadbc6427b130b2e
Ky2510 <rinaldirizqifauzi@gmail.com>
2024-01-10 20:44:51 +0700

    Async,Promises & Ajax include testimonial clear commit

--- assets/javascript/testimonialNew.js
+++ assets/javascript/testimonialNew.js
@@ -1,119 +1,208 @@
-// const datas = [
-//     {
-//         author: "Rinaldi Rizqi Fauzi",
-//         title: "JavaScript",
-//         content: "Lorem Ipsum Dolor",
-//         rating: 1,
-//     },
-//     {
-//         author: "Rinaldi Rizqi Fauzi",
-//         image: "https://e0.pxfuel.com/wallpapers/280/885/desktop-wallpaper-typescript-thumbnail.jpg",
-//         title: "TypeScript",
-//         content: "Lorem Ipsum Dolor",
-//         rating: 2,
-//     },
-//     {
-//         author: "Rinaldi Rizqi Fauzi",
-//         image: "https://blog.appsignal.com/_next/image?url=%2Fimages%2Fblog%2F2023-02%2Fnode-tools.png&w=3840&q=50",
-//         title: "Node JS",
-//         content: "Lorem Ipsum Dolor",
-//         rating: 3,
-//     },
-// ];
+function datas() {
+    // Promise itu objek yang dapat 2 kondisi terpenuhi atau tidak terpenuhi dalam 
+    // operasi asynchronous jika terpenuhi maka akan tampil datanya.
+    // jika tidak terpenuhi maka akan mengeluarkan notifikasi gagal
+    return new Promise((ditepati, diiingkari) => {
+        const connection = new XMLHttpRequest() // XMLHttpRequest itu objek terhubung dengan server melalui request
+        connection.open("GET", "https://api.npoint.io/a1643e1460e5119a3af8", true) // membuka server untuk mengambil data
+        // dimana kode dibawah onload itu sebuah proses yang dapat di akses jika kondisi dipenuhi atau tidak
+        connection.onload = () => {
+            if (connection.status === 200) {
+                const response = JSON.parse(connection.response)
+                ditepati(response)
+            } else {
+                diiingkari('Error loading data')
+            }
+        }
+        // validasi ketika data tidak terpenuhi
+        connection.onerror = () => {
+            diiingkari("Network Error!")
+        }
+        // pengiriman data
+        connection.send()
+    })
+}
 
-const datas = "https://mocki.io/v1/35ebc135-93ef-4d79-9f8a-c687128129f1"
+// membutuhkan waktu untuk menyelesaikan dalam pengambilan data di pengambilan data
+async function DataMain() {
+    const all = document.getElementById("rating-button")
+    all.addEventListener("click", function() {
+        location.reload();
+    });
+    
+    // document.getElementById("card-content").innerHTML = "LOADING..."
+    const contents = await datas()
 
-fetch(datas)
-  .then(response => response.json())
-  .then(datas => {
-    // Lakukan sesuatu dengan datas yang diperoleh
-    console.log(datas);
-  })
-  .catch(error => {
-    console.error("Error fetching data:", error)
-  })
+    let contentHtml = ""
+    let starContent
 
-
-function DataMain() {
-    let dataHTML = ""
-    datas.forEach((val) => {
-        dataHTML += `
-                        <div class="card">
+    contents.forEach((val) => {
+        if (val.rating === 1) {
+            starContent =  `
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                               `
+        }else if(val.rating === 2){
+            starContent =  `
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                `
+        }else if(val.rating === 3){
+            starContent =   `
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                `
+        }else if(val.rating === 4){
+            starContent =   `
+                                    <i class="fa-solid fa-star"></i>
+                                    <i class="fa-solid fa-star"></i>
+                                `
+        }else{
+            starContent =   `
+                                    <i class="fa-solid fa-star"></i>
+                                `
+        }
+        contentHtml += `
+                        <div class="card" style="display: flex; flex-direction: column;">
                             <img src="${val.image}" alt="Foto" class="image">
                             <div class="card-content">
-                                <h4 class="card-text">${val.title} #${val.rating}</h4> 
+                                <h4 class="card-text">${val.title} #${val.rating}
+                                    <div id="star" style="display: flex;">
+                                        ${starContent}
+                                    </div>
+                                </h4>
                                 <div class="description">
-                                    <p class="card-text">${val.content}</p>
+                                    <p class="card-text" style="text-align: center">${val.content}</p>
                                 </div>
-                                <div class="author">
-                                    ${val.author}
+                                <div class="card-footer" style=" display: flex; justify-content: space-between; align-items: flex-end;">
+                                    <div class="author">
+                                        ${val.author}
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    `
-    });
     
-    document.getElementById("card-content").innerHTML = dataHTML;
+                        `
+    })
+
+    document.getElementById("card-content").innerHTML = contentHtml
 }
 
-function FilterRating(rating) {
+async function FilterRating(rating) {
+    // document.getElementById("card-content").innerHTML = "LOADING..."
+
+    const contents = await datas()
+
     const all = document.getElementById('rating-button')
     const one = document.getElementById('rating-button-1')
     const two = document.getElementById('rating-button-2')
     const three = document.getElementById('rating-button-3')
     const four = document.getElementById('rating-button-4')
-    const cariRating = datas.filter((val) => val.rating == rating)
+
+    
+    const filteredTestimonial = contents.filter((value) => value.rating === rating)
+    let star = ""
     if (rating == 1) {
         one.classList.add("active-rating")
         two.classList.remove("active-rating")
         three.classList.remove("active-rating")
         four.classList.remove("active-rating")
+        all.classList.remove("active-rating")
+        for (let value = 0; value < filteredTestimonial.length; value++) {
+            star += `
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>    
+                    `
+        }
     }else if (rating == 2) {
         one.classList.remove("active-rating")
         two.classList.add("active-rating")
         three.classList.remove("active-rating")
         four.classList.remove("active-rating")
+        for (let value = 0; value < filteredTestimonial.length; value++) {
+            star += `
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    `
+        }
     }else if (rating == 3) {
         one.classList.remove("active-rating")
         two.classList.remove("active-rating")
         three.classList.add("active-rating")
         four.classList.remove("active-rating")
+        for (let value = 0; value < filteredTestimonial.length; value++) {
+            star += `
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    `
+        }
     }else if (rating == 4) {
         one.classList.remove("active-rating")
         two.classList.remove("active-rating")
         three.classList.remove("active-rating")
         four.classList.add("active-rating")
+        for (let value = 0; value < filteredTestimonial.length; value++) {
+            star += `
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    `
+        }
     }else {
         one.classList.remove("active-rating")
         two.classList.remove("active-rating")
         three.classList.remove("active-rating")
         four.classList.remove("active-rating")
-        all.classList.add("active-rating")
+        for (let value = 0; value < filteredTestimonial.length; value++) {
+            star += `
+                    <i class="fa-solid fa-star"></i>
+                    `
+        }
     }
 
+    if (!filteredTestimonial.length) {
+        return document.getElementById("card-content").innerHTML = "<h3>Data tidak ditemukan</h3>"
+    }
     
-    
-    let sudahDiFilterHTML = ""
-    cariRating.forEach((val) => {
-        sudahDiFilterHTML += `
-                                    <div class="card">
+    let filteredTestimonialHTML = ""
+     
+    filteredTestimonial.forEach((val) => {
+        filteredTestimonialHTML +=  `
+                                    <div class="card" style="display: flex; flex-direction: column;">
                                         <img src="${val.image}" alt="Foto" class="image">
                                         <div class="card-content">
-                                            <h4 class="card-text">${val.title} #${val.rating}</h4> 
+                                            <h4 class="card-text">${val.title} #${val.rating}
+                                                <div id="star" style="display: flex;">
+                                                    ${star}
+                                                </div>
+                                            </h4>
                                             <div class="description">
-                                                <p class="card-text">${val.content}</p>
+                                                <p class="card-text" style="text-align: center">${val.content}</p>
                                             </div>
-                                            <div class="author">
-                                                ${val.author}
+                                            <div class="card-footer" style=" display: flex; justify-content: space-between; align-items: flex-end;">
+                                                <div class="author">
+                                                    ${val.author}
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                `
+                                    `
     })
 
-    document.getElementById("card-content").innerHTML = sudahDiFilterHTML;
+
+    document.getElementById("card-content").innerHTML = filteredTestimonialHTML
+
 }
-
-
 
 DataMain()
